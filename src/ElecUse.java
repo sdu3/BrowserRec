09058890152d72802bf715e2804c5b68527cdb7e
sarah du <sdu17@deerfield.edu>
2016-01-06 12:18:13 -0500

    Added function to address CPU speed, # of cores and CPU brand. Edited cost of computer code. Started code to process size of screen data.

--- src/ElecUse.java
+++ src/ElecUse.java
@@ -3,15 +3,27 @@ import java.util.Scanner;
 
 public class ElecUse
 {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter the cost of your candidate computer:");
-	int comCost = scan.nextInt();
-	System.out.println comCost;
-
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		//computer cost based on user input from question 1
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the cost of your candidate computer:");
+		int comCost = scan.nextInt();
+		System.out.println("Computer cost = " + comCost);
 
+		//electricity cost based on size of screen 
+		System.out.println("Enter the cost of your candidate computer:");
+		int comCost = scan.nextInt();
+		System.out.println("Computer cost = " + comCost);
+	}
+	
+	public int CpuSpeedCore(String CPUspeed, int numberCores, String CPUbrand)
+	{
+		int CpuSpeedCore;
+		if (speed="low" && cores=2 && brand="Intel") {
+			then CpuSpeedCore=55;
+		}
+		return CpuSpeedCore;
 	}
 
 }
