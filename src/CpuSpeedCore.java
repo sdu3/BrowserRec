b888c3fdefb6e82145c248763082b53c2348c81a
sarah du <sdu17@deerfield.edu>
2016-01-05 21:59:39 -0500

    initial commit by Sarah, already wrote some code

---
+++ src/CpuSpeedCore.java
@@ -0,0 +1,26 @@
+
+public class CpuSpeedCore
+{
+		private String speed;
+		private int cores;
+		private String brand;
+		private int classWatts;
+	
+		public CpuSpeedCore(String CPUspeed, int numberCores, String CPUbrand)
+		{
+			speed=CPUspeed;
+			cores=numberCores;
+			brand=CPUbrand;
+		}
+		
+		{
+			if (speed="low" && cores=2 && brand="Intel"):
+				then classWatts=55;
+			//list all scenarios (total of 7)
+		}
+				
+		public int getclassWatts()
+		{
+			return classWatts;
+		}
+}
---
+++ src/ElecUse.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ElecUse
+{
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the cost of your candidate computer");
+	int comCost = scan.nextLine();
+	System.out.println comCost;
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
