92f129529bbf503f299aa38c228665221bedc3b8
sarah du <sdu17@deerfield.edu>
2016-01-05 22:04:55 -0500

    changed scanner function for integer, used scan.nextLine instead of scan.nextInt

--- src/ElecUse.java
+++ src/ElecUse.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class ElecUse
 {
 	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter the cost of your candidate computer");
-	int comCost = scan.nextLine();
+	System.out.println("Enter the cost of your candidate computer:");
+	int comCost = scan.nextInt();
 	System.out.println comCost;
 
 	public static void main(String[] args)
