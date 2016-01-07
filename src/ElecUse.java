1297765e3b548d98a69cbb5ece3d36ddcea552c0
sarah du <sdu17@deerfield.edu>
2016-01-06 19:57:25 -0500

    added how long the user will use the computer + amount of money a mouse would take

--- src/ElecUse.java
+++ src/ElecUse.java
@@ -18,6 +18,37 @@ public class ElecUse
 		//electricity cost based on size of hard disk
 		System.out.println("Enter the size of hard disk in inches (please enter either 2.5 or 3.5): ");
 		double diskSize = scan.nextDouble();
+		
+		//taking in how long the user will use the computer
+		System.out.println("How many years do you expect to keep your computer? (please enter an integer) ");
+		int years = scan.nextInt();
+		int months = years*12;
+		System.out.println("On average, how many hours a day do you leave the power on? (please enter an integer) ");
+		int hours = scan.nextInt();
+		
+		//considering mouse battery
+		boolean mouse=false;
+		System.out.println("Does your mouse need a battery? (enter no if you don't have a mouse): ");
+		//problem: accurately calculates mBattery but does not allow user input, 
+		//thus this doesn't work for a user without a mouse
+		String yn = scan.nextLine();
+		if (yn=="yes")
+		{
+			mouse=true;
+		}
+		
+		int mBattery;
+		mBattery=0;
+	
+		if (mouse=true)
+		{
+			mBattery=2*(months-2);
+			if (mBattery<=0)
+			{
+				mBattery=0;
+			}
+			System.out.println(mBattery);
+		}
 	}
 	
 	public int CpuSpeedCore(String CPUspeed, int numberCores, String CPUbrand)
